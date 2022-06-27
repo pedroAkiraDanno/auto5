ad255bec313e395c7058a0781d3c2451295273d6
PedroAkira <pedro-akira@hotmail.com>
2022-06-27 17:02:37 -0300

    test and fix: add file and folder oci_provider

--- terraform/oci/oci_provider/oci_provider.tf
+++ terraform/oci/oci_provider/oci_provider.tf
@@ -1,10 +1,10 @@
 # Variables.
-variable "tenancy_ocid"         { type = string sensitive = true }
-variable "user_ocid"            { type = string sensitive = true }
-variable "private_key_path"     { type = string sensitive = true }
-variable "fingerprint"          { type = string sensitive = true }
-variable "region"               { type = string sensitive = true }
-variable "root_compartment_id"  { type = string sensitive = true }
+variable "tenancy_ocid"         { type = string }
+variable "user_ocid"            { type = string }
+variable "private_key_path"     { type = string }
+variable "fingerprint"          { type = string }
+variable "region"               { type = string }
+variable "root_compartment_id"  { type = string }
 
 
 # Resources
