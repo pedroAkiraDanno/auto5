f7ff8337567bc38350bb2922ca8e9c64e79913e0
Pedro Akira <pedro-akira@hotmail.com>
2022-09-22 17:56:08 -0300

    azure add new test about terraform 5

--- srv/terraform/azure/a/VM/04/main.tf
+++ srv/terraform/azure/a/VM/04/main.tf
@@ -69,8 +69,20 @@ resource "azurerm_virtual_machine" "example" {
   }
 }
 
+
+
+
+
+
+
+
+
+
+
+
 resource "azurerm_managed_disk" "example" {
-  name                 = "${local.vm_name}-disk1"
+  count = 2
+  name                 = "${local.vm_name}-disk${count.index + 1}" 
   location             = azurerm_resource_group.example.location
   resource_group_name  = azurerm_resource_group.example.name
   storage_account_type = "Standard_LRS"
@@ -79,28 +91,8 @@ resource "azurerm_managed_disk" "example" {
 }
 
 resource "azurerm_virtual_machine_data_disk_attachment" "example" {
-  managed_disk_id    = azurerm_managed_disk.example.id
-  virtual_machine_id = azurerm_virtual_machine.example.id
-  lun                = "10"
-  caching            = "ReadWrite"
-}
-
-
-
-
-
-
-resource "azurerm_managed_disk" "example1" {
-  name                 = "${local.vm_name}-disk2"
-  location             = azurerm_resource_group.example.location
-  resource_group_name  = azurerm_resource_group.example.name
-  storage_account_type = "Standard_LRS"
-  create_option        = "Empty"
-  disk_size_gb         = 10
-}
-
-resource "azurerm_virtual_machine_data_disk_attachment" "example1-disk" {
-  managed_disk_id    = azurerm_managed_disk.example1.id
+  count=2
+  managed_disk_id    = azurerm_managed_disk.example[count.index].id
   virtual_machine_id = azurerm_virtual_machine.example.id
   lun                = "10"
   caching            = "ReadWrite"
