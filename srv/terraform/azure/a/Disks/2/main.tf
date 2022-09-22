c7eaa60e2c59fe11f17951b7553f399b61b222d7
Pedro Akira <pedro-akira@hotmail.com>
2022-09-22 19:02:30 -0300

    azure add new test about terraform 3

--- srv/terraform/azure/a/Disks/2/main.tf
+++ srv/terraform/azure/a/Disks/2/main.tf
@@ -20,9 +20,9 @@ resource "azurerm_managed_disk" "example" {
   disk_size_gb         = 10
 }
 
-resource "azurerm_virtual_machine_data_disk_attachment" "example" {
-  managed_disk_id    = azurerm_managed_disk.example.id
-  virtual_machine_id = azurerm_virtual_machine.example.id
-  lun                = "10"
-  caching            = "ReadWrite"
-}
+
+
+
+
+
+
