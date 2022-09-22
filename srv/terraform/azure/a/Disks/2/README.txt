5cd66862fd47525a9343768780cf733a8c52edd3
Pedro Akira <pedro-akira@hotmail.com>
2022-09-22 19:00:04 -0300

    azure add new test about terraform 2

---
+++ srv/terraform/azure/a/Disks/2/README.txt
@@ -0,0 +1,4 @@
+
+
+
+REFERENCE: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_machine_data_disk_attachment
--- srv/terraform/azure/a/Disks/2/main.tf
+++ srv/terraform/azure/a/Disks/2/main.tf
@@ -1,5 +1,28 @@
-resource "google_compute_disk" "tst-disk1" {
-    name         = "tst-disk1"
-    zone         = "us-west1-a"
-    type         = "pd-balanced"
+variable "prefix" {
+  default = "example"
+}
+
+locals {
+  vm_name = "${var.prefix}-vm"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_managed_disk" "example" {
+  name                 = "${local.vm_name}-disk1"
+  location             = azurerm_resource_group.example.location
+  resource_group_name  = azurerm_resource_group.example.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 10
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.example.id
+  virtual_machine_id = azurerm_virtual_machine.example.id
+  lun                = "10"
+  caching            = "ReadWrite"
 }
