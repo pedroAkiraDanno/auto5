b519a2a1ae4c730919e51a8eb0b5df5060d1782b
Pedro Akira <pedro-akira@hotmail.com>
2022-09-26 18:44:01 -0300

    add new lines about azure

--- srv/terraform/azure/a/Disks/1/main.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_disk" "tst-disk1" {
-    name         = "tst-disk1"
-    image        = "debian-cloud/debian-10-buster-v20210217"
-    zone         = "us-central1-a"
-    type         = "pd-balanced"
-}
