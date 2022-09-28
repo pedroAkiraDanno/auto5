68e81ae5f9b05ae21f3ae23169259f2053801d5b
Pedro Akira <pedro-akira@hotmail.com>
2022-09-28 01:31:36 -0300

    azure test disk 2

--- srv/terraform/azure/a/VM/02/outputs.tf
+++ srv/terraform/azure/a/VM/02/outputs.tf
@@ -10,3 +10,9 @@ output "tls_private_key" {
   value     = tls_private_key.example_ssh.private_key_pem
   sensitive = true
 }
+
+
+output "tls_public_key" {
+  value     = tls_public_key.example_ssh.public_key_pem
+  sensitive = true
+}
