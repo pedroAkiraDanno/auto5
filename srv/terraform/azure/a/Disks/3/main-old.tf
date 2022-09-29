4699b4963ce069dc7215d5ce828da41588013421
Pedro Akira <pedro-akira@hotmail.com>
2022-09-29 17:35:06 -0300

    azure add StandardSSD_LRS ssd to disks and ubuntu 22.04

--- srv/terraform/azure/a/Disks/3/main-old.tf
+++ srv/terraform/azure/a/Disks/3/main-old.tf
@@ -15,7 +15,7 @@ resource "azurerm_managed_disk" "main" {
   name                 = "${local.vm_name}-disk1"
   location             = azurerm_resource_group.main.location
   resource_group_name  = azurerm_resource_group.main.name
-  storage_account_type = "StandardSSD_LRS"
+  storage_account_type = "Standard_LRS"
   create_option        = "Empty"
   disk_size_gb         = 200
 }
--- srv/terraform/azure/a/VM/05/main-old.tf
+++ srv/terraform/azure/a/VM/05/main-old.tf
@@ -91,7 +91,7 @@ resource "azurerm_linux_virtual_machine" "main" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
