8cce4f4da051c8416b7168897241f8b22cefac6a
Pedro Akira <pedro-akira@hotmail.com>
2022-11-07 22:00:16 -0300

    aws: about s3 bucket test random name

---
+++ srv/terraform/aws/d/buckets/main-random_name.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "random_id" "my-random-id" {
+byte_length = 8
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "s3_bucket-${random_id.my-random-id.dec}"  
+}
+
+resource "aws_s3_bucket_public_access_block" "s3_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
